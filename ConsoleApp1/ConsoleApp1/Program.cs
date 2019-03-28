60faa8c61b0368b66e98055f1413fb2b20e7736f
visualstudio <visualstudio@230-PC41>
2019-03-28 16:49:26 -0400

    commit at 4.49 pm

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("downloading file");
+            Console.ReadLine();
+        }
+
+        static void Download()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("download complete!!@!");
+        }
+    }
+}
